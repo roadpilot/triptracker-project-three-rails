a5b64be1948c7974572d74d9c5a279af87805ea3
mdacht777 <mdacht777@gmail.com>
2020-10-14 14:12:25 -0600

    non-resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :trips
   resources :locations
   resources :users
+  root 'application#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

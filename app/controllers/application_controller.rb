39d7c8b47ba6cfbcd50825cd58b6f3a6cbcfa74c
mdacht777 <mdacht777@gmail.com>
2020-10-14 21:05:14 -0600

    current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
   end
+
+  helper_method :current_user
+  def current_user
+    User.find(session[:user_id])
+  end
 end
 

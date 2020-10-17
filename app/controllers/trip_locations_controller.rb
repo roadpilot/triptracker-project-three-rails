ae935de13716b064b4cd89328371d98995eecd35
mdacht777 <mdacht777@gmail.com>
2020-10-17 09:39:38 -0600

    triplocations controller update

--- app/controllers/trip_locations_controller.rb
+++ app/controllers/trip_locations_controller.rb
@@ -1,2 +1,13 @@
 class TripLocationsController < ApplicationController
+    def update
+        # binding.pry
+        tl = TripLocation.find_by(id: params[:id])
+        tl.update(tl_params)
+        redirect_to trip_path(tl.trip_id)
+    end
+
+    def tl_params
+        params.require(:trip_location).permit(:time_in, :time_out)
+    end
+
 end

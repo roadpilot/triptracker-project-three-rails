e6b09d5ef9338f05807edae66e29a122de0d8412
mdacht777 <mdacht777@gmail.com>
2020-10-17 11:17:59 -0600

    destroy trip location

--- app/controllers/trip_locations_controller.rb
+++ app/controllers/trip_locations_controller.rb
@@ -6,6 +6,12 @@ class TripLocationsController < ApplicationController
         redirect_to trip_path(tl.trip_id)
     end
 
+    def destroy
+        tl = TripLocation.find_by(id: params[:id])
+        tl.destroy
+        redirect_to trip_path(tl.trip_id)
+    end
+
     def tl_params
         params.require(:trip_location).permit(:time_in, :time_out)
     end

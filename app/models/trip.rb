132f9dafbe9e1e55ba1d504da157430625081662
mdacht777 <mdacht777@gmail.com>
2020-10-17 16:54:02 -0600

    implement disp_name method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,13 @@ class Trip < ApplicationRecord
     # belongs_to :location, optional: true
     has_many :trip_locations
     has_many :locations, through: :trip_locations
+
+    def disp_name
+        if self.name == ""
+            self.created_at
+        else
+            self.name
+        end
+    end
+    
 end

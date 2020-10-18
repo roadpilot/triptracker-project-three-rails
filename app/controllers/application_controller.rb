b62a7afc342317f519ea6a44bf8876ec7f0c49f7
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2020-10-18 09:27:14 -0600

    moved helper methods into helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ApplicationHelper
 
   def index
     redirect_to "/"
-  end
-
-  helper_method :current_user
-  def current_user
-    current_user ||= User.find_by_id(session[:user_id])
-  end
-
-  helper_method :logged_in?
-  def logged_in?
-    !!current_user
-  end
-
-  helper_method :require_logged_in
-  def require_logged_in
-    return head(:forbidden) unless session.include? :user_id
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def current_user
+    current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    return head(:forbidden) unless session.include? :user_id
+  end
+
 end

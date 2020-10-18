6e785091d5bbc2cd6d2f70fee7176e8befb8f89f
mdacht777 <mdacht777@gmail.com>
2020-10-17 18:35:20 -0600

    tightening up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  resources :trip_locations
-  resources :comments
-  resources :trips
-  resources :locations
-  resources :users
-  resources :users do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :trip_locations, only: [:show,:create,:destroy]
+  # resources :comments  FOR FUTURE USE
+  resources :trips, only: [:index,:show,:new,:create,:destroy]
+  resources :locations, only: [:index,:create]
+  # resources :users, only: []
+
+  resources :users, only: [:show,:new,:create] do
     resources :trips, only: [:new]
   end
+
   root 'sessions#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   post '/logout' => 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-
 end
